c92d8b1aee04b47a0e95e48c5e0403533970e842
Israel Ekpo <isekpo@microsoft.com>
2026-04-21 12:46:18 -0400

    adding chained agent example

---
+++ realtime-voice-example/bot_websocket_server.py
@@ -0,0 +1,132 @@
+#
+# Copyright (c) 2024–2026, Daily
+#
+# SPDX-License-Identifier: BSD 2-Clause License
+#
+
+import os
+
+from pipecat.services.azure.llm import AzureLLMService
+from pipecat.services.azure.stt import AzureSTTService
+from pipecat.services.azure.tts import AzureTTSService
+
+from loguru import logger
+from pipecat.audio.vad.silero import SileroVADAnalyzer
+from pipecat.frames.frames import LLMRunFrame
+from pipecat.pipeline.pipeline import Pipeline
+from pipecat.pipeline.runner import PipelineRunner
+from pipecat.pipeline.task import PipelineParams, PipelineTask
+from pipecat.processors.aggregators.llm_context import LLMContext
+from pipecat.processors.aggregators.llm_response_universal import (
+    LLMContextAggregatorPair,
+    LLMUserAggregatorParams,
+)
+from pipecat.serializers.protobuf import ProtobufFrameSerializer
+from pipecat.transports.websocket.server import (
+    WebsocketServerParams,
+    WebsocketServerTransport,
+)
+
+SYSTEM_INSTRUCTION = f"""
+"You are An Azure Voice Chatbot, a friendly, helpful robot.
+
+Your goal is to demonstrate your capabilities in a succinct way.
+
+Your output will be converted to audio so don't include special characters in your answers.
+
+Respond to what the user said in a creative and helpful way. Keep your responses brief. One or two sentences at most.
+"""
+
+azure_api_key = os.getenv("AZURE_SPEECH_API_KEY", "")
+azure_region = os.getenv("AZURE_SPEECH_REGION", "")
+azure_openai_api_key = os.getenv("AZURE_OPENAI_API_KEY", "")
+azure_openai_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT", "")
+azure_openai_deployment = os.getenv("AZURE_OPENAI_DEPLOYMENT", "")
+
+async def run_bot_websocket_server():
+    ws_transport = WebsocketServerTransport(
+        params=WebsocketServerParams(
+            serializer=ProtobufFrameSerializer(),
+            audio_in_enabled=True,
+            audio_out_enabled=True,
+            add_wav_header=False,
+            session_timeout=60 * 3,  # 3 minutes
+        )
+    )
+
+    # Initialize services
+    # Speech-to-text
+    stt = AzureSTTService(api_key=azure_api_key, region=azure_region)
+
+    # LLM agent
+    llm = AzureLLMService(
+        api_key=azure_openai_api_key,
+        endpoint=azure_openai_endpoint,
+        model=azure_openai_deployment,
+    )
+
+    # Text-to-speech
+    tts = AzureTTSService(
+        api_key=azure_api_key,
+        region=azure_region,
+        voice="en-US-AriaNeural",  # Default Azure voice
+    )
+
+    context = LLMContext(
+        [
+            {
+                "role": "user",
+                "content": "Start by greeting the user warmly and introducing yourself.",
+            }
+        ],
+    )
+    user_aggregator, assistant_aggregator = LLMContextAggregatorPair(
+        context,
+        user_params=LLMUserAggregatorParams(
+            vad_analyzer=SileroVADAnalyzer(),
+        ),
+    )
+
+    pipeline = Pipeline(
+        [
+            ws_transport.input(),
+            user_aggregator,
+            stt,
+            llm,  # LLM
+            tts,
+            ws_transport.output(),
+            assistant_aggregator,
+        ]
+    )
+
+    task = PipelineTask(
+        pipeline,
+        params=PipelineParams(
+            enable_metrics=True,
+            enable_usage_metrics=True,
+        ),
+    )
+
+    @task.rtvi.event_handler("on_client_ready")
+    async def on_client_ready(rtvi):
+        logger.info("Pipecat client ready.")
+        # Kick off the conversation.
+        await task.queue_frames([LLMRunFrame()])
+
+    @ws_transport.event_handler("on_client_connected")
+    async def on_client_connected(transport, client):
+        logger.info("Pipecat Client connected")
+
+    @ws_transport.event_handler("on_client_disconnected")
+    async def on_client_disconnected(transport, client):
+        logger.info("Pipecat Client disconnected")
+        await task.cancel()
+
+    @ws_transport.event_handler("on_session_timeout")
+    async def on_session_timeout(transport, client):
+        logger.info(f"Entering in timeout for {client.remote_address}")
+        await task.cancel()
+
+    runner = PipelineRunner()
+
+    await runner.run(task)
